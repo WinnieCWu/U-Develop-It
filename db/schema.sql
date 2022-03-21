90a93f7e898eb5df7931732dcf39f99861c20a0b
WinnieCWu <winniechn7@gmail.com>
2022-03-20 22:15:36 -0700

    added vote table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
@@ -24,4 +25,18 @@ CREATE TABLE voters (
   email VARCHAR(50) NOT NULL,
   /* capure when voter registered*/
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  /*set up constraints to prevent duplicate records or 
+  counting votes when the candidate or voter isn't in db*/
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  /*uc_voter signifies that vlues inserted into the id field must be unique*/
+  CONSTRAINT fk_voters FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  /*ON DELETE CASCADE = deleting the ref key will also delete entire row from this table*/
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
